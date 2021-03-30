3ecb358e02082d300b015c4739d3e24bc1c21ca5
cristian1920 <cristian131411@gmail.com>
2021-03-30 12:35:07 -0500

    agregamos index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Trabajo De clase IUMAFIS</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <header>
+        <div class="row">
+            <div class="col-md-4"> </div>
+            <div class="col-md-4">
+                <h1><b>
+                        <p style="color:skyblue;"> Trabajo en clase</p>
+                    </b></h1>
+            </div>
+            <div class="col-md-4"> </div>
+        </div>
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+            <div class="panel panel-default">
+                <div class="panel-heading"><img src="../lenguaje/imagenes/1.png" /> USO DE TECNOLOGIA WEB- PHP</div>
+                <div class="panel-body">
+                    <div class="col-md-2"><img src="../lenguaje/imagenes/2.png" /> <br><a href="taller1.php" style="text-decoration:none;color:black;(otros)"> Ejercicio 1 - Formulario</a></br></div>
+                    <div class="col-md-2"> <img src="../lenguaje/imagenes/3.png" /> <br><a href="funciones.php" style="text-decoration:none;color:black;(otros)"> Ejercicio 2 - Arreglos y Funciones</a></br></div>
+                    <div class="col-md-2"><img src="../lenguaje/imagenes/4.png" /> <br><a href="metodoget.php" style="text-decoration:none;color:black;(otros)"> Ejercicio 3 - Metodo Get</a></br></div>
+                    <div class="col-md-2"><img src="../lenguaje/imagenes/5.png" /> <br><a href="metodopost.php" style="text-decoration:none;color:black;(otros)"> Ejercicio 4 - Metodo Post</a></br></div>
+                    <div class="col-md-2"> <img src="../lenguaje/imagenes/7.png" /> <br><a href="conexion/basedatos.php" style="text-decoration:none;color:black;(otros)"> Crud- base de datos </a></br></div>
+                    <div class="col-md-2"><img src="../lenguaje/imagenes/6.png" /> <br><a href="site/crud.php" style="text-decoration:none;color:black;(otros)">
+                     Crud</a></br></div>
+                     
+                      <div class="col-md-2"><img src="../lenguaje/imagenes/8.png" /> <br><a href="../lenguaje/modal/login.php" style="text-decoration:none;color:black;(otros)"> Parcial - Login</a></br></div>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-md-2"></div>
+        <div class="row">
+            <div class="col-md-4">*</div>
+            <div class="col-md-4">
+                <img src="imagenes/unnamed.png" />
+            </div>
+            <div class="col-md-4">*</div>
+        </div>
+    </header>
+
+
+</body>
+
+</html>
